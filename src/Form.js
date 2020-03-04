a290c9779a9bfa22df7031e5200e3b38441c0aaa
Sharna Hossain <sharnajhossain@gmail.com>
2020-03-03 20:31:38 -0500

    favicon change

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { IoMdRocket, IoIosCopy } from "react-icons/io";
+import { IoMdRocket, IoIosCopy, IoMdCheckmarkCircle } from "react-icons/io";
+import anime from "animejs/lib/anime.es.js";
 
 class Form extends Component {
   state = {
@@ -29,14 +30,59 @@ class Form extends Component {
   send = e => {
     e.preventDefault();
   };
+  toggleClipboardBubble = toggle => {
+    if (toggle) {
+      this.refs.bubble.innerHTML = "Copy to Clipboard?";
+      anime({
+        targets: "#bubble",
+        opacity: [0, 1],
+        color: "#830095",
+        duration: 200,
+        easing: "linear"
+      });
+      anime({
+        targets: "#clipboardwrapper",
+        color: "#00b7ff",
+        easing: "linear",
+        duration: 200
+      });
+    } else {
+      anime({
+        targets: "#bubble",
+        opacity: [1, 0],
+        duration: 100,
+        easing: "linear"
+      });
+      anime({
+        targets: "#clipboardwrapper",
+        color: "#fff",
+        easing: "linear",
+        duration: 100
+      });
+    }
+  };
   copyToClipboard = () => {
     const email = "sharnajh@gmail.com";
     navigator.clipboard.writeText(email).then(
       () => {
         console.log("Async: Copying to clipboard was successful!");
+        this.refs.bubble.innerHTML = "Successfully copied to clipboard!";
+        anime({
+          targets: "#bubble",
+          color: "#830095",
+          easing: "linear",
+          duration: 0
+        });
       },
       err => {
         console.error("Async: Could not copy text: ", err);
+        this.refs.bubble.innerHTML = "Your browser does not support clipboard copying.";
+        anime({
+          targets: "#bubble",
+          color: "#ff1a82",
+          easing: "linear",
+          duration: 0
+        });
       }
     );
   };
@@ -74,8 +120,14 @@ class Form extends Component {
         </button>
         <div id="clipboardwrapper">
           <a href="mailto:sharnajh@gmail.com">sharnajh@gmail.com</a>{" "}
-          <div id="clipboard">
-            <div id="bubble">Copy to clipboard?</div>
+          <div
+            id="clipboard"
+            onMouseEnter={() => this.toggleClipboardBubble(true)}
+            onMouseLeave={() => this.toggleClipboardBubble(false)}
+          >
+            <div id="bubble" ref="bubble">
+              Copy to clipboard?
+            </div>
             <IoIosCopy onClick={this.copyToClipboard} />
           </div>
         </div>
